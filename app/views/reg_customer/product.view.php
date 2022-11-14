00cbd8d52192b5d2ac59d487ca3971aa385b0641
Danodya Supun <danodyasupun7@gmail.com>
2022-11-14 23:01:02 +0530

    View product Backend completed.

--- app/views/reg_customer/product.view.php
+++ app/views/reg_customer/product.view.php
@@ -3,14 +3,20 @@
 <div class="product-desc-section">
     <div class="product-img">
         <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="product image">
+        <div class="product-sub-img">
+            <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="product image">
+            <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="product image">
+        </div>
     </div>
     <div class="product-desc">
         <h2><?=$row[0]->Name?></h2>
         <h1>Rs. <?=$row[0]->Cost?>.00</h1>
-        <h2>Wood Type : <?=$row[0]->Wood_type?></h2>
+        <p>Wood Type : <?=$row[0]->Wood_type?></p>
         <p><?=$row[0]->Description?></p>
-        <p><?=$row[0]->Availability == 1 ? "In Stock" : "Out of Stock";?></p>
-
+        <div class="product-details">
+            <p class="product-detail"><?=$row[0]->Availability == 1 ? "In Stock" : "Out of Stock";?></p>
+            <p class="product-detail"><?=$row[0]->Warrenty_period?> Warrenty</p>
+        </div>
         <div>
             <button>Add to cart</button>
         </div>
