463312bf75db00d224386722f090dfd4fccaddae
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-11-16 18:59:29 +0530

    Changed table order

--- app/configs/database.php
+++ app/configs/database.php
@@ -264,6 +264,32 @@ class Database
         $this->query($query);
 
         $query = "
+             	CREATE TABLE IF NOT EXISTS `order` (
+                 `OrderID` char(60) NOT NULL,
+                 `Payment_type` char(6) NOT NULL,
+                 `Total_amount` float NOT NULL,
+                 `Date` timestamp NOT NULL DEFAULT current_timestamp(),
+                 `Deliver_method` char(8) NOT NULL,
+                 `Order_status` char(10) NOT NULL,
+                 `Address` varchar(200) NOT NULL,
+                 `CustomerID` char(60) NOT NULL,
+                 `DriverID` char(5) NOT NULL,
+                 PRIMARY KEY (`OrderID`),
+                 KEY `OrderID` (`OrderID`),
+                 KEY `Payment_type` (`Payment_type`),
+                 KEY `Total_amount` (`Total_amount`),
+                 KEY `Date` (`Date`),
+                 KEY `Deliver_method` (`Deliver_method`),
+                 KEY `Order_status` (`Order_status`),
+                 KEY `Address` (`Address`),
+                 KEY `CustomerID` (`CustomerID`),
+                 KEY `DriverID` (`DriverID`),
+                 CONSTRAINT `CustomerOrder` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE NO ACTION ON UPDATE CASCADE
+                ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+        ";
+
+        $this->query($query);
+        $query = "
             	CREATE TABLE IF NOT EXISTS `issues` (
                  `IssueID` char(60) NOT NULL,
                  `Problem_statement` varchar(1024) NOT NULL,
@@ -296,32 +322,6 @@ class Database
 
         $this->query($query);
 
-        $query = "
-             	CREATE TABLE IF NOT EXISTS `order` (
-                 `OrderID` char(60) NOT NULL,
-                 `Payment_type` char(6) NOT NULL,
-                 `Total_amount` float NOT NULL,
-                 `Date` timestamp NOT NULL DEFAULT current_timestamp(),
-                 `Deliver_method` char(8) NOT NULL,
-                 `Order_status` char(10) NOT NULL,
-                 `Address` varchar(200) NOT NULL,
-                 `CustomerID` char(60) NOT NULL,
-                 `DriverID` char(5) NOT NULL,
-                 PRIMARY KEY (`OrderID`),
-                 KEY `OrderID` (`OrderID`),
-                 KEY `Payment_type` (`Payment_type`),
-                 KEY `Total_amount` (`Total_amount`),
-                 KEY `Date` (`Date`),
-                 KEY `Deliver_method` (`Deliver_method`),
-                 KEY `Order_status` (`Order_status`),
-                 KEY `Address` (`Address`),
-                 KEY `CustomerID` (`CustomerID`),
-                 KEY `DriverID` (`DriverID`),
-                 CONSTRAINT `CustomerOrder` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE NO ACTION ON UPDATE CASCADE
-                ) ENGINE=InnoDB DEFAULT CHARSET=latin1
-        ";
-
-        $this->query($query);
 
         $query = "
              	CREATE TABLE IF NOT EXISTS `order_item` (
