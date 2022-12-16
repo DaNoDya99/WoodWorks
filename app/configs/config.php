4c29b2ae21a4ffff8fdeea81f48fe87c7af54b74
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-12-16 22:17:18 +0530

    Revert "Added gitignore for config.php"
    This reverts commit 2771dd0be3b8e36c59ad361429c680a04b92f979.

--- app/configs/config.php
+++ app/configs/config.php
@@ -7,7 +7,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
     define('DBHOST','localhost');
     define('DBNAME','woodworks');
     define('DBUSER','root');
-    define('DBPASS','root');
+    define('DBPASS','');
     define('DBDRIVER','mysql');
     define('ROOT','http://localhost/WoodWorks/public');
 
