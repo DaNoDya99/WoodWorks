001778a2ca9121425ccce32979ac9e33c2dcbbf4
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-12-19 11:25:33 +0530

    Updated Cashier Dash

--- app/views/cashier/dash.view.php
+++ app/views/cashier/dash.view.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,7 +45,7 @@
                             <td style=" width:200px; word-wrap: break-word;"><?= $product->Name ?></td>
                             <td><?= $product->Quantity ?></td>
                             <td><?= $product->Cost ?></td>
-                            <td><a href="<?= ROOT ?>/cashier/add_to_cart/<?= $product->ProductID ?>" style="border-radius: 100px; border:0; background-color: green; padding:10px; color:white; height:40px; width:40px;">+</a></td>
+                            <td><a href="<?= ROOT ?>/cashier/add_to_cart/<?= $product->ProductID ?>"><img style="width: 16px;" src="<?= ROOT ?>/assets/images/cashier/add.svg" alt=""></a></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
@@ -54,50 +53,43 @@
         </div>
     </div>
     <div class="sec3">
-        <div class="panel2" style="color: white; padding:10px">
+        <div class="panel2">
             <h3>
                 Orders
             </h3>
-            <table style="font-size:14px">
-                <?php if(!empty($data['cart'])):?>
-                    <?php foreach ($data['cart'] as $cart) : ?>
-                        <tr>
-                            <td>
 
-                                <?= $cart->Name; ?>
-                                <?= $cart->ProductID; ?>
-                            
-                                <a href="<?=ROOT?>/cashier/removeItem/<?=$cart->CartID?>/<?=$cart->ProductID?>/<?=$cart->Cost?>/<?=$cart->Quantity?>">Remove</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                <p>Cart is empty</p>
+            <div style="height: 500px;">
+                <?php if (!empty($data['cart'])) : ?>
+                    <ul>
+                        <?php foreach ($data['cart'] as $cart) : ?>
+                            <li class="pos-item">
+                                <div>
+                                    <?= $cart->Name; ?><br>
+                                    <small><?= $cart->ProductID; ?></small>
+                                </div>
+                                <div style="display:flex; justify-content:center;align-items:center;">
+                                    <a href="<?= ROOT ?>/cashier/increaseQuantity/<?= $cart->CartID ?>/<?= $cart->ProductID ?>/<?= $cart->Quantity ?>/<?= $cart->Cost ?>"><img style="width: 16px;" src="<?= ROOT ?>/assets/images/cashier/add2.svg" alt=""></a>
+                                    <input type="text" value="<?= $cart->Quantity ?>">
+                                    <!-- <?= $cart->Quantity; ?> -->
+                                    <a href="<?= ROOT ?>/cashier/decreaseQuantity/<?= $cart->CartID ?>/<?= $cart->ProductID ?>/<?= $cart->Quantity ?>/<?= $cart->Cost ?>"><img style="width: 16px;" src="<?= ROOT ?>/assets/images/cashier/minus.svg" alt=""></a>
+
+                                </div>
+                                <a href="<?= ROOT ?>/cashier/removeItem/<?= $cart->CartID ?>/<?= $cart->ProductID ?>/<?= $cart->Cost ?>/<?= $cart->Quantity ?>">X</a>
+                            </li>
+
+
+                        <?php endforeach; ?>
+                    </ul>
+                <?php else : ?>
+                    <p>Cart is empty</p>
                 <?php endif ?>
-            </table>
+            </div>
+            <div>
+                <button class="proceed">Proceed to Payment</button>
+            </div>
+
         </div>
     </div>
 </body>
-<!-- <script>
-    function timedelload() {
-        setTimeout(function() {
-            document.getElementById("panel").style.opacity = 1;
-            document.getElementById("panel").style.marginTop = "75px";
-        }, 200);
-    }
-
-    function timedelexit() {
-        setTimeout(function() {
-            document.getElementById("panel").style.opacity = 0;
-            document.getElementById("panel").style.marginTop = "150px";
-        }, 200);
-    }
-
-    function delay(URL) {
-        setTimeout(function() {
-            window.location = URL
-        }, 500);
-    }
-</script> -->
 
 </html>
