0323003a31eb191bd61fc6aea8f42374a3609675
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-12-19 06:32:45 +0530

    Cashier Nav component

---
+++ app/views/cashier/cashier.nav.view.php
@@ -0,0 +1,43 @@
+<nav>
+    <img class="logo" src="<?= ROOT ?>/assets/images/supplier/WOODWORKS.svg" alt="">
+    <ul>
+        <li>
+            <a href="javascript:delay('<?= ROOT ?>/cashier')">
+
+                <div onclick="timedelexit()">
+                 Order List
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:delay('<?= ROOT ?>/cashier/billing')">
+                <div onclick="timedelexit()">
+                    Order </div>
+            </a>
+        </li>
+        <li>
+        <li>
+            <a href="javascript:delay('<?= ROOT ?>/cashier/inventory')">
+                <div onclick="timedelexit()">
+                    Inventory </div>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:delay('<?= ROOT ?>/cashier/profile')">
+                <div onclick="timedelexit()">
+                    View Profile
+                </div>
+            </a>
+        </li>
+        <!-- <li class="subnav">
+            <a href="javascript:delay('<?= ROOT ?>/supplier/editprofile')">
+                Edit Profile
+            </a>
+        </li> -->
+
+    </ul>
+
+</nav>
+<script>
+
+</script>
