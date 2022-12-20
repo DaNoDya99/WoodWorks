61120f951fedb73adc911f836fc88c6f35be8cc8
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-12-20 07:19:30 +0530

    New functions for cashier

--- app/controllers/cashier.php
+++ app/controllers/cashier.php
@@ -53,7 +53,14 @@ class cashier extends Controller
         $cart->updateTotalAmountToIncrease($cartID, $cost);
         $this->redirect('cashier/dash');
     }
-
+    public function completebill(){
+        $cart = new Carts();
+        $order_item = new Order_Items();
+        $q = "DELETE FROM `order_item` WHERE CartID = :CartID;";
+        $cart_id = $cart->getCart('5lhHfqCRtAdbMxdEl69oEq1F0ywitBClYh3fF927If44CB4eaXFKGSgp4K0k')[0]->CartID;
+        $order_item->query($q,['CartID' => $cart_id]);
+        $this->redirect('cashier/dash');
+    }
 
     public function Profile($id = null)
     {
@@ -82,8 +89,8 @@ class cashier extends Controller
                     if ($_FILES['Image']['error'] == 0) {
                         if (in_array($_FILES['Image']['type'], $allowedFileType)) {
                             $destination = $folder . time() . $_FILES['Image']['name'];
-                            move_uploaded_file($_FILES['Image']['tmp_name'], $destination);
-
+                            show(move_uploaded_file($_FILES['Image']['tmp_name'], $destination));
+                            die;
                             //                            resize_image($destination);
                             $_POST['Image'] = $destination;
                             if (file_exists($row[0]->Image)) {
