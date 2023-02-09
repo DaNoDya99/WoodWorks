a26bd934fb5f2124e477f443fdceb7db8e4a7519
viharshaj <84612797+viharshaj@users.noreply.github.com>
2023-02-09 15:21:47 +0530

    Designs details page UI completed

--- app/views/manager/design_details.view.php
+++ app/views/manager/design_details.view.php
@@ -1,28 +1,27 @@
 <?php $this->view('manager/includes/header') ?>
 
-<body>
+<body class="manager">
+<?php $this->view('manager/includes/manager_header') ?>
+    <div class="content manager-body ">
+        <div class="design-details-container">
+            <div class="design-image-slider">
+                <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="Design Image">
+                <h2>1/3</h2>
 
-<div class="card">
-<div class="product-card-img">
-    <!-- <img src="<?=ROOT?>/<?=$row->Image?>" alt="Design Image"> -->
-</div>
-<span><?=$designs[0]->Name?></span><br>
-<span><?=$designs[0]->Description?></span><br>
-<span><?=$designs[0]->DesignerID?></span><br>
-<span><?=$designs[0]->Date?></span><br> -->
-
-</div>
-
-
-
-
-<!-- <span><?=$designs[0]->DesignID?></span><br>
-<span><?=$designs[0]->Name?></span><br>
-<span><?=$designs[0]->Description?></span><br>
-<span><?=$designs[0]->DesignerID?></span><br>
-<span><?=$designs[0]->Date?></span><br> -->
-
-
-    
+                <a class="prev" onclick="plusSlides(-1)">&#10094</a>
+                <a class="next" onclick="plusSlides(1)">&#10095</a>
+            </div>
+            <div class="design-details">
+                <h1>Wooden Study Table</h1>
+                <span>DES00027</span>
+                <p>Flaunting the contemporary vibes with sleek designing, the Mcbeth dining table comes with four spacious storage drawers and an extra dose of elegance. To serve for many years, this dining table is crafted from Sheesham wood. The wooden grained patterns are making the table more attractive and beautiful, it is further available in different wooden finishes</p>
+            
+                <div class="designs-btn-container">
+                    <button class="design-accept">Accept</button>
+                    <button class="design-reject">Reject</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
