53d1c915fd6bea2d7c84d6ad1265dd8544385599
Danodya Supun <89889362+DaNoDya99@users.noreply.github.com>
2023-02-09 20:09:05 +0530

    Order details and order history UI completed

--- app/views/reg_customer/orders.view.php
+++ app/views/reg_customer/orders.view.php
@@ -5,17 +5,265 @@
         <h1>Order History</h1>
         <div class="order-details">
             <div class="lhs-details">
-                <h4>#d45efg5</h4>
-                <span>Rs. 110500.00</span>
-                <span>5 Items</span>
+                <h4 class="lhs-details-item">#d45efg5</h4>
+                <span class="lhs-details-item">Rs. 110500.00</span>
+                <span class="lhs-details-item">5 Items</span>
             </div>
             <div class="rhs-details">
-
+                <span>Feb 5, at 3.30 PM</span>
+                <div class="progress">
+                    <img src="<?=ROOT?>/assets/images/customer/delivery-svgrepo-com.svg" alt="Vehicle">
+                    <span class="progress">In progress</span>
+                </div>
+                <div class="delivery-status-container">
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                </div>
+            </div>
+        </div>
+        <div class="order-details">
+            <div class="lhs-details">
+                <h4 class="lhs-details-item">#d45efg5</h4>
+                <span class="lhs-details-item">Rs. 110500.00</span>
+                <span class="lhs-details-item">5 Items</span>
+            </div>
+            <div class="rhs-details">
+                <span>Feb 5, at 3.30 PM</span>
+                <div class="progress">
+                    <img src="<?=ROOT?>/assets/images/customer/delivery-svgrepo-com.svg" alt="Vehicle">
+                    <span class="progress">In progress</span>
+                </div>
+                <div class="delivery-status-container">
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                </div>
+            </div>
+        </div>
+        <div class="order-details">
+            <div class="lhs-details">
+                <h4 class="lhs-details-item">#d45efg5</h4>
+                <span class="lhs-details-item">Rs. 110500.00</span>
+                <span class="lhs-details-item">5 Items</span>
+            </div>
+            <div class="rhs-details">
+                <span>Feb 5, at 3.30 PM</span>
+                <div class="progress">
+                    <img src="<?=ROOT?>/assets/images/customer/delivery-svgrepo-com.svg" alt="Vehicle">
+                    <span class="progress">In progress</span>
+                </div>
+                <div class="delivery-status-container">
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                </div>
+            </div>
+        </div>
+        <div class="order-details">
+            <div class="lhs-details">
+                <h4 class="lhs-details-item">#d45efg5</h4>
+                <span class="lhs-details-item">Rs. 110500.00</span>
+                <span class="lhs-details-item">5 Items</span>
+            </div>
+            <div class="rhs-details">
+                <span>Feb 5, at 3.30 PM</span>
+                <div class="progress">
+                    <img src="<?=ROOT?>/assets/images/customer/delivery-svgrepo-com.svg" alt="Vehicle">
+                    <span class="progress">In progress</span>
+                </div>
+                <div class="delivery-status-container">
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                </div>
+            </div>
+        </div>
+        <div class="order-details">
+            <div class="lhs-details">
+                <h4 class="lhs-details-item">#d45efg5</h4>
+                <span class="lhs-details-item">Rs. 110500.00</span>
+                <span class="lhs-details-item">5 Items</span>
+            </div>
+            <div class="rhs-details">
+                <span>Feb 5, at 3.30 PM</span>
+                <div class="progress">
+                    <img src="<?=ROOT?>/assets/images/customer/delivery-svgrepo-com.svg" alt="Vehicle">
+                    <span class="progress">In progress</span>
+                </div>
+                <div class="delivery-status-container">
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                    <div class="delivery-status"></div>
+                </div>
             </div>
         </div>
     </div>
     <div class="order">
         <h1>Order</h1>
+        <div class="order-items-container">
+            <div class="order-progressing-bar">
+                <div class="prog-status-container">
+                    <img src="<?=ROOT?>/assets/images/customer/dollar-circle-svgrepo-com(1).svg" alt="paid">
+                    <span>Paid</span>
+                    <img class="completed" src="<?=ROOT?>/assets/images/customer/tick-circle-svgrepo-com.svg" alt="">
+                </div>
+                <div class="line line-active"></div>
+                <div class="prog-status-container">
+                    <img src="<?=ROOT?>/assets/images/customer/gift-box-svgrepo-com.svg" alt="paid">
+                    <span>Processing</span>
+                    <img class="completed" src="<?=ROOT?>/assets/images/customer/tick-circle-svgrepo-com.svg" alt="">
+                </div>
+                <div class="line line-active"></div>
+                <div class="prog-status-container">
+                    <img src="<?=ROOT?>/assets/images/customer/shipping-svgrepo-com.svg" alt="paid">
+                    <span>Dispatched</span>
+                    <img class="current" src="<?=ROOT?>/assets/images/customer/loading-part-2-svgrepo-com.svg" alt="">
+                </div>
+                <div class="line"></div>
+                <div class="prog-status-container">
+                    <img class="yet-to-complete" src="<?=ROOT?>/assets/images/customer/delivered-svgrepo-com.svg" alt="paid">
+                    <span class="yet-to-complete">Delivered</span>
+                </div>
+                <div class="line"></div>
+                <div class="prog-status-container">
+                    <img class="yet-to-complete" src="<?=ROOT?>/assets/images/customer/review-like-message-svgrepo-com.svg" alt="paid">
+                    <span class="yet-to-complete">Review</span>
+                </div>
+            </div>
+            <div class="order-items">
+                <div class="order-item">
+                    <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="">
+
+                    <div class="ordered-product-details">
+                        <div class="ordered-product-details-lhs">
+                            <div class="row1">
+                                <h4>Study Table</h4>
+                                <span>P0001</span>
+                            </div>
+
+                            <div class="row2"><span>Teak</span></div>
+                            <div class="row3"><span>1 item</span></div>
+                        </div>
+
+                        <div class="price">
+                            <span>Rs.45000.00</span>
+                        </div>
+                    </div>
+
+                </div>
+                 <div class="order-item">
+                    <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="">
+
+                    <div class="ordered-product-details">
+                        <div class="ordered-product-details-lhs">
+                            <div class="row1">
+                                <h4>Study Table</h4>
+                                <span>P0001</span>
+                            </div>
+
+                            <div class="row2"><span>Teak</span></div>
+                            <div class="row3"><span>1 item</span></div>
+                        </div>
+
+                        <div class="price">
+                            <span>Rs.45000.00</span>
+                        </div>
+                    </div>
+
+                </div>
+                 <div class="order-item">
+                    <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="">
+
+                    <div class="ordered-product-details">
+                        <div class="ordered-product-details-lhs">
+                            <div class="row1">
+                                <h4>Study Table</h4>
+                                <span>P0001</span>
+                            </div>
+
+                            <div class="row2"><span>Teak</span></div>
+                            <div class="row3"><span>1 item</span></div>
+                        </div>
+
+                        <div class="price">
+                            <span>Rs.45000.00</span>
+                        </div>
+                    </div>
+
+                </div>
+                 <div class="order-item">
+                    <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="">
+
+                    <div class="ordered-product-details">
+                        <div class="ordered-product-details-lhs">
+                            <div class="row1">
+                                <h4>Study Table</h4>
+                                <span>P0001</span>
+                            </div>
+
+                            <div class="row2"><span>Teak</span></div>
+                            <div class="row3"><span>1 item</span></div>
+                        </div>
+
+                        <div class="price">
+                            <span>Rs.45000.00</span>
+                        </div>
+                    </div>
+
+                </div>
+                 <div class="order-item">
+                    <img src="<?=ROOT?>/assets/images/customer/chair.jpg" alt="">
+
+                    <div class="ordered-product-details">
+                        <div class="ordered-product-details-lhs">
+                            <div class="row1">
+                                <h4>Study Table</h4>
+                                <span>P0001</span>
+                            </div>
+
+                            <div class="row2"><span>Teak</span></div>
+                            <div class="row3"><span>1 item</span></div>
+                        </div>
+
+                        <div class="price">
+                            <span>Rs.45000.00</span>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
+            <div class="order-payment-details">
+                <h2>Order Details</h2>
+                <div class="order-detail">
+                    <h4>Phone Number</h4>
+                    <span>0766023645</span>
+                </div>
+                <div class="order-detail">
+                    <h4>Delivery Address</h4>
+                    <span>108/5 A, Weragama Road, Wadduwa</span>
+                </div>
+                <div class="order-detail order-final-detail">
+                    <h4>Invoice Number</h4>
+                    <span>12345</span>
+                </div>
+                <div class="order-detail order-total">
+                    <h4>Total Amount</h4>
+                    <span>Rs. 110500.00</span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
