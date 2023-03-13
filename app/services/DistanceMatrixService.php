869eecc9f286bbfedef0e90a54466c73f40bcc16
Danodya Supun <89889362+DaNoDya99@users.noreply.github.com>
2023-03-13 19:31:16 +0530

    Distance Metrix API Configured Successfully.

--- app/services/DistanceMatrixService.php
+++ app/services/DistanceMatrixService.php
@@ -4,13 +4,16 @@ class DistanceMatrixService
 {
     public function calculateDistance($origin, $destination) {
 
-        $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=$origin&destinations=$destination&key=".DISTANCE_MATRIX_API_KEY;
+        $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=$origin&destinations=$destination&mode=walking&key=".DISTANCE_MATRIX_API_KEY;
         $response = file_get_contents($url);
         $result = json_decode($response, true);
 
-        $distance = $result['rows'][0]['elements'][0]['distance']['value'];
-        $duration = $result['rows'][0]['elements'][0]['duration']['value'];
+        $distance = $result['rows'][0]['elements'][0]['distance']['text'];
+        $duration = $result['rows'][0]['elements'][0]['duration']['text'];
+        $destination_addresses = $result['destination_addresses'][0];
+        $origin_addresses = $result['origin_addresses'][0];
+        $status = $result['status'];
 
-        return array('distance' => $distance, 'duration' => $duration);
+        return array('destination' => $destination_addresses,'origin' => $origin_addresses,'distance' => $distance, 'duration' => $duration, 'status' => $status);
     }
 }
