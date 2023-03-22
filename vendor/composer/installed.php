546a84c95d8c13997ae7816d31a98c50f2f6f2d2
Nisura Indisa <nisuraindisa21@gmail.com>
2023-03-22 09:26:39 +0530

    Added Stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '538d98a8ec9fe1c35e1dea529708f7a6df3bd379',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '538d98a8ec9fe1c35e1dea529708f7a6df3bd379',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.10.0',
+            'version' => '10.10.0.0',
+            'reference' => '2c641079093167c2e448d82bd0bd02160e20fec1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
